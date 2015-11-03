f55e44540a68f5c7d9eb05afbccd2b9cffb4c5d4
Arun Gimblett <gimblett44@gmail.com>
2015-11-03 16:35:21 +0800

    bug fixed

--- fem1d.c
+++ fem1d.c
@@ -642,7 +642,7 @@ void assemble ( double adiag[], double aleft[], double arite[], double f[],
                 }
             }
         }
-    }
+    
     return;
 }
 /******************************************************************************/
